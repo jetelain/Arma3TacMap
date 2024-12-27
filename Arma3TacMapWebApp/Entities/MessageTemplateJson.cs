240762efdb0380fb920a7f3783250a4b75cf52bb
Julien <julien.etelain@outlook.com>
2024-12-27 19:45:34 +0100

    Import Message Template Json

--- Arma3TacMapWebApp/Entities/MessageTemplateJson.cs
+++ Arma3TacMapWebApp/Entities/MessageTemplateJson.cs
@@ -1,20 +1,29 @@
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace Arma3TacMapWebApp.Entities
 {
     public class MessageTemplateJson
     {
-        public string Uid { get; set; }
+        public string? Uid { get; set; }
 
-        public List<MessageLineTemplate> Lines { get; set; }
+        public List<MessageLineTemplate>? Lines { get; set; }
 
-        public string Title { get; set; }
+        public string? Title { get; set; }
 
         public string? Description { get; set; }
 
+        [JsonConverter(typeof(JsonStringEnumConverter<MessageTemplateType>))]
         public MessageTemplateType Type { get; set; }
 
-        public string Href { get; set; }
+        public string? Href { get; set; }
+
+        public string? CountryCode { get; set; }
+
+        public MessageTemplateJson()
+        {
+
+        }
 
         public MessageTemplateJson(MessageTemplate messageTemplate, string uid, string href)
         {
@@ -24,6 +33,7 @@ namespace Arma3TacMapWebApp.Entities
             Description = messageTemplate.Description;
             Type = messageTemplate.Type;
             Href = href;
+            CountryCode = messageTemplate.CountryCode;
         }
     }
 }
